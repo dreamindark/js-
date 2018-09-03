8bef84d60528b145865e29b79f793f1aae440453
dreamindark <1441396922@qq.com>
2018-09-03 10:16:08 +0800

    Update roll.js

--- roll.js
+++ roll.js
@@ -11,9 +11,7 @@ function roll(ele,target) {
         }
     },25);
 }
-/**
- * Created by andy on 2015/12/8.
- */
+
 function scroll() {  // 开始封装自己的scrollTop
     if(window.pageYOffset != null) {  // ie9+ 高版本浏览器
         // 因为 window.pageYOffset 默认的是  0  所以这里需要判断
@@ -33,14 +31,8 @@ function scroll() {  // 开始封装自己的scrollTop
         top: document.body.scrollTop
     }
 }
-/**
- * Created by Lenovo on 2016/9/2.
- */
-/**
- * 通过传递不同的参数获取不同的元素
- * @param str
- * @returns {*}
- */
+
+
 function $(str){
     var str1 = str.charAt(0);
     if(str1==="#"){
@@ -52,58 +44,33 @@ function $(str){
     }
 }
 
-/**
- * 功能：给定元素查找他的第一个元素子节点，并返回
- * @param ele
- * @returns {Element|*|Node}
- */
+
 function getFirstNode(ele){
     var node = ele.firstElementChild || ele.firstChild;
     return node;
 }
 
-/**
- * 功能：给定元素查找他的最后一个元素子节点，并返回
- * @param ele
- * @returns {Element|*|Node}
- */
+
 function getLastNode(ele){
     return ele.lastElementChild || ele.lastChild;
 }
 
-/**
- * 功能：给定元素查找他的下一个元素兄弟节点，并返回
- * @param ele
- * @returns {Element|*|Node}
- */
+
 function getNextNode(ele){
     return ele.nextElementSibling || ele.nextSibling;
 }
 
-/**
- * 功能：给定元素查找他的上一个兄弟元素节点，并返回
- * @param ele
- * @returns {Element|*|Node}
- */
+
 function getPrevNode(ele){
     return ele.previousElementSibling || ele.previousSibling;
 }
 
-/**
- * 功能：给定元素和索引值查找指定索引值的兄弟元素节点，并返回
- * @param ele 元素节点
- * @param index 索引值
- * @returns {*|HTMLElement}
- */
+
 function getEleOfIndex(ele,index){
     return ele.parentNode.children[index];
 }
 
-/**
- * 功能：给定元素查找他的所有兄弟元素，并返回数组
- * @param ele
- * @returns {Array}
- */
+
 function getAllSiblings(ele){
     //定义一个新数组，装所有的兄弟元素，将来返回
     var newArr = [];
